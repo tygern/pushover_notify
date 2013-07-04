d3e9e4d61c8687ff3e68023728cba98087093bc4
tygern <tygern@gmail.com>
2013-07-04 14:56:46 -0600

    Don't pass device to message.

--- lib/pushover_notify/message.rb
+++ lib/pushover_notify/message.rb
@@ -2,14 +2,13 @@ module PushoverNotify
   class Message
     attr_accessor :text, :title, :url, :url_title, :sound, :priority
 
-    def initialize(text: '', title: nil, url: nil, url_title: nil, sound: nil, priority: nil, device: nil)
+    def initialize(text: '', title: nil, url: nil, url_title: nil, sound: nil, priority: nil)
       @text = text
       @title = title
       @url = url
       @url_title = url_title
       @sound = sound
       @priority = priority
-
     end
   end
 end
